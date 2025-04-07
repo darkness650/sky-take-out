ec6e6af4f62b405239ee60abfa508863e6932d8c
darkness650 <873319973@qq.com>
2025-04-07 21:43:47 +0800

    苍穹外卖项目初始代码

---
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -0,0 +1,27 @@
+package com.sky;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement //开启注解方式的事务管理
+@Slf4j
+public class SkyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkyApplication.class, args);
+        log.info("server started");
+    }
+    @Bean
+    public CommandLineRunner testConfig(Environment env) {
+        return args -> {
+            System.out.println("Database URL: " + env.getProperty("spring.datasource.url"));
+            System.out.println("DB Host: " + env.getProperty("sky.datasource.host"));
+        };
+    }
+}
